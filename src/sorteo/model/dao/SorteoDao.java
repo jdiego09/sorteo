729900920595ca7d6ea467612595c9c4fcd72883
jdiego09 <>
2018-04-27 17:21:29 -0600

    Gestión de sorteos

--- src/sorteo/model/dao/SorteoDao.java
+++ src/sorteo/model/dao/SorteoDao.java
@@ -23,147 +23,139 @@ import sorteo.utils.TipoResultado;
  */
 public class SorteoDao extends BaseDao<Integer, Sorteo> {
 
-    private static SorteoDao INSTANCE;
-    private Sorteo sorteo;
+   private static SorteoDao INSTANCE;
+   private Sorteo sorteo;
 
-    private SorteoDao() {
-    }
+   private SorteoDao() {
+   }
 
-    private static void createInstance() {
-        if (INSTANCE == null) {
-            // Sólo se accede a la zona sincronizada
-            // cuando la instancia no está creada
-            synchronized (SorteoDao.class) {
-                // oDEn la zona sincronizada sería necesario volver
-                // a comprobar que no se ha creado la instancia
-                if (INSTANCE == null) {
-                    INSTANCE = new SorteoDao();
-                }
+   private static void createInstance() {
+      if (INSTANCE == null) {
+         // Sólo se accede a la zona sincronizada
+         // cuando la instancia no está creada
+         synchronized (SorteoDao.class) {
+            // oDEn la zona sincronizada sería necesario volver
+            // a comprobar que no se ha creado la instancia
+            if (INSTANCE == null) {
+               INSTANCE = new SorteoDao();
             }
-        }
-    }
+         }
+      }
+   }
 
-    public static SorteoDao getInstance() {
-        if (INSTANCE == null) {
-            createInstance();
-        }
-        return INSTANCE;
-    }
+   public static SorteoDao getInstance() {
+      if (INSTANCE == null) {
+         createInstance();
+      }
+      return INSTANCE;
+   }
 
-    public void setSorteo(Sorteo sorteo) {
-        this.sorteo = sorteo;
-    }
+   public void setSorteo(Sorteo sorteo) {
+      this.sorteo = sorteo;
+   }
 
-    //para que solamente exista una instancia del objeto
-    @Override
-    public Object clone() throws CloneNotSupportedException {
-        throw new CloneNotSupportedException();
-    }
+   //para que solamente exista una instancia del objeto
+   @Override
+   public Object clone() throws CloneNotSupportedException {
+      throw new CloneNotSupportedException();
+   }
 
-    public Resultado<Sorteo> findByCodigo(Integer codigoSorteo) {
-        Sorteo existe;
-        Resultado<Sorteo> resultado = new Resultado<>();
-        try {
-            Query query = getEntityManager().createNamedQuery("Sorteo.findByCodigo");
-            query.setParameter("codigo", codigoSorteo);
-            existe = (Sorteo) query.getSingleResult();
+   public Resultado<Sorteo> findByCodigo(Integer codigoSorteo) {
+      Sorteo existe;
+      Resultado<Sorteo> resultado = new Resultado<>();
+      try {
+         Query query = getEntityManager().createNamedQuery("Sorteo.findByCodigo");
+         query.setParameter("codigo", codigoSorteo);
+         existe = (Sorteo) query.getSingleResult();
 
-            resultado.setResultado(TipoResultado.SUCCESS);
-            resultado.set(existe);
-            return resultado;
-        } catch (NoResultException nre) {
-            Logger.getLogger(SorteoDao.class.getName()).log(Level.SEVERE, null, nre);
-            resultado.setResultado(TipoResultado.ERROR);
-            resultado.setMensaje("No existe el sorteo con el código [" + String.valueOf(codigoSorteo) + "].");
-            return resultado;
-        } catch (Exception ex) {
-            Logger.getLogger(SorteoDao.class.getName()).log(Level.SEVERE, null, ex);
-            resultado.setResultado(TipoResultado.ERROR);
-            resultado.setMensaje("Error consultando sorteo con el código [" + String.valueOf(codigoSorteo) + "].");
-            return resultado;
-        }
-    }
+         resultado.setResultado(TipoResultado.SUCCESS);
+         resultado.set(existe);
+         return resultado;
+      } catch (NoResultException nre) {
+         Logger.getLogger(SorteoDao.class.getName()).log(Level.SEVERE, null, nre);
+         resultado.setResultado(TipoResultado.ERROR);
+         resultado.setMensaje("No existe el sorteo con el código [" + String.valueOf(codigoSorteo) + "].");
+         return resultado;
+      } catch (Exception ex) {
+         Logger.getLogger(SorteoDao.class.getName()).log(Level.SEVERE, null, ex);
+         resultado.setResultado(TipoResultado.ERROR);
+         resultado.setMensaje("Error consultando sorteo con el código [" + String.valueOf(codigoSorteo) + "].");
+         return resultado;
+      }
+   }
 
-    public Resultado<Sorteo> findByFecha(Date fechaSorteo, TipoSorteo tipoSorteo) {
-        Sorteo existe;
-        Resultado<Sorteo> resultado = new Resultado<>();
-        try {
-            Query query = getEntityManager().createNamedQuery("Sorteo.findByFecha");
-            query.setParameter("fechaSorteo", fechaSorteo);
-            query.setParameter("tipoSorteo", tipoSorteo.getCodigo());
-            existe = (Sorteo) query.getSingleResult();
+   public Resultado<Sorteo> findByFecha(Date fechaSorteo, TipoSorteo tipoSorteo) {
+      Sorteo existe;
+      Resultado<Sorteo> resultado = new Resultado<>();
+      try {
+         Query query = getEntityManager().createNamedQuery("Sorteo.findByFecha");
+         query.setParameter("fechaSorteo", fechaSorteo);
+         query.setParameter("tipoSorteo", tipoSorteo.getCodigo());
+         existe = (Sorteo) query.getSingleResult();
 
-            resultado.setResultado(TipoResultado.SUCCESS);
-            resultado.set(existe);
-            return resultado;
-        } catch (NoResultException nre) {
-            Logger.getLogger(SorteoDao.class.getName()).log(Level.SEVERE, null, nre);
-            resultado.setResultado(TipoResultado.ERROR);
-            resultado.setMensaje("No existe el sorteo [" + tipoSorteo.getDescripcion() + "] para la fecha [" + fechaSorteo + "].");
-            return resultado;
-        } catch (Exception ex) {
-            Logger.getLogger(SorteoDao.class.getName()).log(Level.SEVERE, null, ex);
-            resultado.setResultado(TipoResultado.ERROR);
-            resultado.setMensaje("Error consultando sorteo con el código [" + tipoSorteo.getDescripcion() + "] para la fecha [" + fechaSorteo + "].");
-            return resultado;
-        }
-    }
+         resultado.setResultado(TipoResultado.SUCCESS);
+         resultado.set(existe);
+         return resultado;
+      } catch (NoResultException nre) {
+         Logger.getLogger(SorteoDao.class.getName()).log(Level.SEVERE, null, nre);
+         resultado.setResultado(TipoResultado.ERROR);
+         resultado.setMensaje("No existe el sorteo [" + tipoSorteo.getDescripcion() + "] para la fecha [" + fechaSorteo + "].");
+         return resultado;
+      } catch (Exception ex) {
+         Logger.getLogger(SorteoDao.class.getName()).log(Level.SEVERE, null, ex);
+         resultado.setResultado(TipoResultado.ERROR);
+         resultado.setMensaje("Error consultando sorteo con el código [" + tipoSorteo.getDescripcion() + "] para la fecha [" + fechaSorteo + "].");
+         return resultado;
+      }
+   }
 
-    public Resultado<Double> getTotalApostadoNumero(Sorteo sorteo, int numero) {
-        Resultado<Double> result = new Resultado<>();
-        Query query = getEntityManager().createNativeQuery("select sum(d.monto)\n"
-        + "  from DetalleFactura d \n"
-        + "  join d.factura f\n"
-        + "  join f.sorteo s\n"
-        + "  join s.tipoSorteo t\n"
-        + " where t.codigo = :codTSorteo\n"
-        + "   and s.codigo = :codSorteo\n"
-        + "   and s.fecha = :fecSorteo\n"
-        + "   and d.numero = :numero");
+   public Resultado<Double> getTotalApostadoNumero(Sorteo sorteo, int numero) {
+      Resultado<Double> result = new Resultado<>();
+      Query query = getEntityManager().createNamedQuery("Sorteo.getMontoApostadoNumero");
 
-        query.setParameter("codTSorteo", sorteo.getTipoSorteo().getCodigo());
-        query.setParameter("codSorteo", sorteo.getCodigo());
-        query.setParameter("fecSorteo", Formater.getInstance().formatFechaDB.format(sorteo.getFecha()));
-        query.setParameter("numero", numero);
-        try {
-            Number resultado = (Number) query.getSingleResult();
-            result.setResultado(TipoResultado.SUCCESS);
-            result.set(resultado.doubleValue());
-            return result;
-        } catch (Exception ex) {
-            Logger.getLogger(SorteoDao.class.getName()).log(Level.SEVERE, null, ex);
-            result.setResultado(TipoResultado.ERROR);
-            result.setMensaje("Error consultar monto apostado por número para el sorteo.");
-            return result;
-        }
-    }
+      query.setParameter("codTSorteo", sorteo.getTipoSorteo().getCodigo());
+      query.setParameter("codSorteo", sorteo.getCodigo());
+      query.setParameter("fecSorteo", Formater.getInstance().formatFechaDB.format(sorteo.getFecha()));
+      query.setParameter("numero", numero);
+      try {
+         Number resultado = (Number) query.getSingleResult();
+         result.setResultado(TipoResultado.SUCCESS);
+         result.set(resultado.doubleValue());
+         return result;
+      } catch (Exception ex) {
+         Logger.getLogger(SorteoDao.class.getName()).log(Level.SEVERE, null, ex);
+         result.setResultado(TipoResultado.ERROR);
+         result.setMensaje("Error consultar monto apostado por número para el sorteo.");
+         return result;
+      }
+   }
 
-    public Resultado<Sorteo> save() {
-        Resultado<Sorteo> result = new Resultado<>();
-        try {
-            if (sorteo.getCodigo() == null || sorteo.getCodigo() <= 0) {
-                //expediente.setExpFechaIngreso(new Date());
-                //expediente.setExpUsuarioingresa(Aplicacion.getInstance().getUsuario().getUssCodigo());
-                //expediente.getExpUsucodigo().setUsuSedcodigo(Aplicacion.getInstance().getDefaultSede());
-                ;
-            } else {
-                /*expediente.setExpFechamodifica(new Date());
+   public Resultado<Sorteo> save() {
+      Resultado<Sorteo> result = new Resultado<>();
+      try {
+         if (sorteo.getCodigo() == null || sorteo.getCodigo() <= 0) {
+            //expediente.setExpFechaIngreso(new Date());
+            //expediente.setExpUsuarioingresa(Aplicacion.getInstance().getUsuario().getUssCodigo());
+            //expediente.getExpUsucodigo().setUsuSedcodigo(Aplicacion.getInstance().getDefaultSede());
+            ;
+         } else {
+            /*expediente.setExpFechamodifica(new Date());
                 expediente.setExpUsuariomodifica(Aplicacion.getInstance().getUsuario().getUssCodigo());
                 expediente.getExpUsucodigo().setUsuUsuariomodifica(Aplicacion.getInstance().getUsuario().getUssCodigo());
                 expediente.getExpUsucodigo().setUsuFechamodifica(new Date());*/
-                ;
-            }
+            ;
+         }
 
-            sorteo = (Sorteo) super.save(sorteo);
-            result.setResultado(TipoResultado.SUCCESS);
-            result.set(sorteo);
-            result.setMensaje("El sorteo se guardó correctamente.");
-            return result;
-        } catch (Exception ex) {
-            Logger.getLogger(SorteoDao.class.getName()).log(Level.SEVERE, null, ex);
-            result.setResultado(TipoResultado.ERROR);
-            result.setMensaje("Error al registrar el sorteo.");
-            return result;
-        }
-    }
+         sorteo = (Sorteo) super.save(sorteo);
+         result.setResultado(TipoResultado.SUCCESS);
+         result.set(sorteo);
+         result.setMensaje("El sorteo se guardó correctamente.");
+         return result;
+      } catch (Exception ex) {
+         Logger.getLogger(SorteoDao.class.getName()).log(Level.SEVERE, null, ex);
+         result.setResultado(TipoResultado.ERROR);
+         result.setMensaje("Error al registrar el sorteo.");
+         return result;
+      }
+   }
 }
--- src/sorteo/model/entities/Sorteo.java
+++ src/sorteo/model/entities/Sorteo.java
@@ -41,130 +41,140 @@ import javax.xml.bind.annotation.XmlTransient;
 @Table(name = "sor_sorteo", schema = "sorteo")
 @XmlRootElement
 @NamedQueries({
-    @NamedQuery(name = "Sorteo.findAll", query = "SELECT s FROM Sorteo s")
-    , @NamedQuery(name = "Sorteo.findByCodigo", query = "SELECT s FROM Sorteo s WHERE s.codigo = :codigo")
-    , @NamedQuery(name = "Sorteo.findByFecha", query = "select e from Sorteo e join e.tipoSorteo t\n"
-            + "where e.fecha = :fechaSorteo\n"
-            + "and t.codigo = :tipoSorteo")})
+   @NamedQuery(name = "Sorteo.findAll", query = "SELECT s FROM Sorteo s")
+   , @NamedQuery(name = "Sorteo.findByCodigo", query = "SELECT s FROM Sorteo s WHERE s.codigo = :codigo")
+   , @NamedQuery(name = "Sorteo.findByFecha", query = "select e from Sorteo e join e.tipoSorteo t\n"
+           + "where e.fecha = :fechaSorteo\n"
+           + "and t.codigo = :tipoSorteo")
+   ,
+@NamedQuery(name = "Sorteo.getMontoApostadoNumero", query = "select sum(d.monto)\n"
+           + "  from DetalleFactura d \n"
+           + "  join d.factura f\n"
+           + "  join f.sorteo s\n"
+           + "  join s.tipoSorteo t\n"
+           + " where t.codigo = :codTSorteo\n"
+           + "   and s.codigo = :codSorteo\n"
+           + "   and s.fecha = :fecSorteo\n"
+           + "   and d.numero = :numero")})
 public class Sorteo implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+   private static final long serialVersionUID = 1L;
 
-    @Transient
-    private Integer codigo;
-    @Transient
-    SimpleObjectProperty<LocalDate> fecha;
-    @OneToMany(mappedBy = "sorteo", fetch = FetchType.LAZY)
-    private List<Factura> listaFacturas;
-    @JoinColumn(name = "sor_codsucursal", referencedColumnName = "suc_codigo")
-    @ManyToOne(fetch = FetchType.LAZY)
-    private Sucursal sucursal;
-    @JoinColumn(name = "sor_tiposorteo", referencedColumnName = "tso_codigo")
-    @ManyToOne(fetch = FetchType.LAZY)
-    private TipoSorteo tipoSorteo;
+   @Transient
+   private Integer codigo;
+   @Transient
+   SimpleObjectProperty<LocalDate> fecha;
+   @OneToMany(mappedBy = "sorteo", fetch = FetchType.LAZY)
+   private List<Factura> listaFacturas;
+   @JoinColumn(name = "sor_codsucursal", referencedColumnName = "suc_codigo")
+   @ManyToOne(fetch = FetchType.LAZY)
+   private Sucursal sucursal;
+   @JoinColumn(name = "sor_tiposorteo", referencedColumnName = "tso_codigo")
+   @ManyToOne(fetch = FetchType.LAZY)
+   private TipoSorteo tipoSorteo;
 
-    public Sorteo() {
-    }
+   public Sorteo() {
+   }
 
-    public Sorteo(Integer sorCodigo) {
-        this.codigo = sorCodigo;
-    }
+   public Sorteo(Integer sorCodigo) {
+      this.codigo = sorCodigo;
+   }
 
-    public Sorteo(Date fecha, TipoSorteo tipoSorteo, Sucursal sucursal) {
-        this.fecha = new SimpleObjectProperty<>();
-        this.setFecha(fecha);
-        this.sucursal = sucursal;
-        this.tipoSorteo = tipoSorteo;
-    }
+   public Sorteo(Date fecha, TipoSorteo tipoSorteo, Sucursal sucursal) {
+      this.fecha = new SimpleObjectProperty<>();
+      this.setFecha(fecha);
+      this.sucursal = sucursal;
+      this.tipoSorteo = tipoSorteo;
+   }
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Basic(optional = false)
-    @Column(name = "sor_codigo")
-    @Access(AccessType.PROPERTY)
-    public Integer getCodigo() {
-        return codigo;
-    }
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   @Basic(optional = false)
+   @Column(name = "sor_codigo")
+   @Access(AccessType.PROPERTY)
+   public Integer getCodigo() {
+      return codigo;
+   }
 
-    public void setCodigo(Integer codigo) {
-        this.codigo = codigo;
-    }
+   public void setCodigo(Integer codigo) {
+      this.codigo = codigo;
+   }
 
-    @Column(name = "sor_fecha")
-    @Temporal(TemporalType.DATE)
-    @Access(AccessType.PROPERTY)
-    public Date getFecha() {
-        if (fecha != null && fecha.get() != null) {
-            return Date.from(fecha.get().atStartOfDay(ZoneId.systemDefault()).toInstant());
-        } else {
-            return null;
-        }
-    }
+   @Column(name = "sor_fecha")
+   @Temporal(TemporalType.DATE)
+   @Access(AccessType.PROPERTY)
+   public Date getFecha() {
+      if (fecha != null && fecha.get() != null) {
+         return Date.from(fecha.get().atStartOfDay(ZoneId.systemDefault()).toInstant());
+      } else {
+         return null;
+      }
+   }
 
-    public SimpleObjectProperty<LocalDate> getFechaProperty() {
-        if (this.fecha == null) {
+   public SimpleObjectProperty<LocalDate> getFechaProperty() {
+      if (this.fecha == null) {
+         this.fecha = new SimpleObjectProperty();
+      }
+      return this.fecha;
+   }
+
+   public void setFecha(Date fecha) {
+      if (fecha != null) {
+         if (this.fecha == null) {
             this.fecha = new SimpleObjectProperty();
-        }
-        return this.fecha;
-    }
+         }
+         this.fecha.set(fecha.toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
+      }
+   }
 
-    public void setFecha(Date fecha) {
-        if (fecha != null) {
-            if (this.fecha == null) {
-                this.fecha = new SimpleObjectProperty();
-            }
-            this.fecha.set(fecha.toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
-        }
-    }
+   @XmlTransient
+   public List<Factura> getListaFacturas() {
+      return listaFacturas;
+   }
 
-    @XmlTransient
-    public List<Factura> getListaFacturas() {
-        return listaFacturas;
-    }
+   public void setListaFacturas(List<Factura> listaFacturas) {
+      this.listaFacturas = listaFacturas;
+   }
 
-    public void setListaFacturas(List<Factura> listaFacturas) {
-        this.listaFacturas = listaFacturas;
-    }
+   public Sucursal getSucursal() {
+      return sucursal;
+   }
 
-    public Sucursal getSucursal() {
-        return sucursal;
-    }
+   public void setSucursal(Sucursal sucursal) {
+      this.sucursal = sucursal;
+   }
 
-    public void setSucursal(Sucursal sucursal) {
-        this.sucursal = sucursal;
-    }
+   public TipoSorteo getTipoSorteo() {
+      return tipoSorteo;
+   }
 
-    public TipoSorteo getTipoSorteo() {
-        return tipoSorteo;
-    }
+   public void setTipoSorteo(TipoSorteo tipoSorteo) {
+      this.tipoSorteo = tipoSorteo;
+   }
 
-    public void setTipoSorteo(TipoSorteo tipoSorteo) {
-        this.tipoSorteo = tipoSorteo;
-    }
+   @Override
+   public int hashCode() {
+      int hash = 0;
+      hash += (codigo != null ? codigo.hashCode() : 0);
+      return hash;
+   }
 
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (codigo != null ? codigo.hashCode() : 0);
-        return hash;
-    }
+   @Override
+   public boolean equals(Object object) {
+      // TODO: Warning - this method won't work in the case the id fields are not set
+      if (!(object instanceof Sorteo)) {
+         return false;
+      }
+      Sorteo other = (Sorteo) object;
+      if ((this.codigo == null && other.codigo != null) || (this.codigo != null && !this.codigo.equals(other.codigo))) {
+         return false;
+      }
+      return true;
+   }
 
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Sorteo)) {
-            return false;
-        }
-        Sorteo other = (Sorteo) object;
-        if ((this.codigo == null && other.codigo != null) || (this.codigo != null && !this.codigo.equals(other.codigo))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "Sorteo{" + "codigo=" + codigo + '}';
-    }
+   @Override
+   public String toString() {
+      return "Sorteo{" + "codigo=" + codigo + '}';
+   }
 
 }
